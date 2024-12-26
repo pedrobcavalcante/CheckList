c1b64e8ef61da9db980c5f82a8597c3b28ef9903
Pedro B Cavalcante <pedrobenevides3243@gmail.com>
2024-12-26 13:16:12 -0300

    justify content center

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,14 +6,19 @@ export const globalStyles = css`
     padding: 0;
     box-sizing: border-box;
   }
-
+  html,
   body {
     font-family: "Roboto", sans-serif;
     background-color: #f5f5f5;
     color: #333;
     line-height: 1.6;
   }
-
+  #root {
+    width: 100%; /* Ocupar todo o espaço */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
   h1 {
     font-size: 2rem;
     color: #0077cc;
