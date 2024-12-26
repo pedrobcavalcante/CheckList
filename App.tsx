e40a851e472ae3f57328bf51b502e4610e7b193c
Pedro B Cavalcante <pedrobenevides3243@gmail.com>
2024-12-26 15:43:57 -0300

    bug fixes

--- App.tsx
+++ App.tsx
@@ -2,14 +2,20 @@
 import { Global } from "@emotion/react";
 import { globalStyles, headerStyle, mainStyle } from "./src/styles/global";
 import AppRoutes from "./src/routes";
+import { useLocation } from "react-router-dom"; // Importação do hook
 
 export default function App() {
+  const location = useLocation(); // Obter a rota atual
+
   return (
     <>
       <Global styles={globalStyles} />
-      <header css={headerStyle}>
-        <h1>My Checklist</h1>
-      </header>
+      {/* Renderiza o cabeçalho apenas se não estiver na página de login */}
+      {location.pathname !== "/" && (
+        <header css={headerStyle}>
+          <h1>My Checklist</h1>
+        </header>
+      )}
       <main css={mainStyle}>
         <AppRoutes />
       </main>
