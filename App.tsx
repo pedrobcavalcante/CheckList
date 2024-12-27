25092e7b7804009cc0d7bcf916197d6705c20f83
Pedro B Cavalcante <pedrobenevides3243@gmail.com>
2024-12-27 15:51:37 -0300

    fix app bar

--- App.tsx
+++ App.tsx
@@ -2,14 +2,20 @@
 import { Global } from "@emotion/react";
 import { globalStyles, headerStyle, mainStyle } from "./src/styles/global";
 import AppRoutes from "./src/routes";
+import { useLocation } from "react-router-dom";
 
 export default function App() {
+  const location = useLocation();
+
   return (
     <>
       <Global styles={globalStyles} />
-      <header css={headerStyle}>
-        <h1>My Checklist</h1>
-      </header>
+      { }
+      {location.pathname !== "/" && (
+        <header css={headerStyle}>
+          <h1>My Checklist</h1>
+        </header>
+      )}
       <main css={mainStyle}>
         <AppRoutes />
       </main>
